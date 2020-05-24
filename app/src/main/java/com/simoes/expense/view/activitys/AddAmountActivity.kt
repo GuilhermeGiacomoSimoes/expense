fe0bf366abc38e6245fc31bbcf4a1fed51fbae43
Guilherme <trintaeoitogc@gmail.com>
2020-05-24 18:38:59 -0300

    creating screens

---
+++ app/src/main/java/com/simoes/expense/view/activitys/AddAmountActivity.kt
@@ -0,0 +1,12 @@
+package com.simoes.expense.view.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddAmountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_amount)
+    }
+}
---
+++ app/src/main/java/com/simoes/expense/view/activitys/PaymentActivity.kt
@@ -0,0 +1,13 @@
+package com.simoes.expense.view.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.simoes.expense.R
+
+class PaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+    }
+}
