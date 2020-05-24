7083d87703313f88572e2ab109947cfc90e73417
Guilherme <trintaeoitogc@gmail.com>
2020-05-24 19:24:18 -0300

    modified confirmanddetailsdialog.kt

--- app/src/main/java/com/simoes/expense/view/fragments/ConfirmAndDetailsDialog.kt
+++ app/src/main/java/com/simoes/expense/view/fragments/ConfirmAndDetailsDialog.kt
@@ -1,10 +1,52 @@
 package com.simoes.expense.view.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import com.simoes.expense.R
 
 class ConfirmAndDetailsDialog : DialogFragment() {
 
-    companion object {
+    private lateinit var title          : String
+    private lateinit var description    : String
+    private lateinit var titleBtnCancel : String
+    private lateinit var titleBtnOk     : String
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.confirm_and_details_dialog, container, false)
+    }
+
+    companion object {
+        private fun getInstance()  = ConfirmAndDetailsDialog()
+
+        fun showDialog(fragmentManager: FragmentManager, title: String, description: String?, titleBtnOk : String?, titleBtnCancel: String?) {
+            with(getInstance()) {
+                if(!isAdded) {
+                    isCancelable = false
+
+                    this.title = title
+                    this.description    = description?
+                    this.titleBtnOk     = titleBtnOk?
+                    this.titleBtnCancel = titleBtnCancel?
+
+                    show(fragmentManager, "")
+                }
+            }
+        }
+
+        fun dismissLoading() {
+            with(getInstance()) {
+                if(isAdded) {
+                    dismiss()
+                }
+            }
+        }
     }
 }
