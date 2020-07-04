937dee5280ed94a880f0402ee6cd45294f9ac24c
Guilherme <trintaeoitogc@gmail.com>
2020-07-04 17:05:26 -0300

    add expene

--- app/src/main/java/com/simoes/expense/model/models/Expense.kt
+++ app/src/main/java/com/simoes/expense/model/models/Expense.kt
@@ -1,14 +1,12 @@
 package com.simoes.expense.model.models
 
 class Expense : Object() {
-
     lateinit var name    : String
     lateinit var dueDate : String
     lateinit var bank    : Bank
-    lateinit var paidBy  : User
 
     var paidOut: Boolean = false
     var isFixed: Boolean = false
     var value: Double    = .0
-
+    var repeat : Boolean = false
 }
--- app/src/main/java/com/simoes/expense/view/activitys/AddExpenseActivity.kt
+++ app/src/main/java/com/simoes/expense/view/activitys/AddExpenseActivity.kt
@@ -6,18 +6,28 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.simoes.expense.R
+import com.simoes.expense.controller.CRUDController
+import com.simoes.expense.helpers.CallBackReturn
+import com.simoes.expense.model.models.Bank
+import com.simoes.expense.model.models.Expense
 import kotlinx.android.synthetic.main.activity_add_expense.*
+import java.util.ArrayList
 
-class AddExpenseActivity : AppCompatActivity() {
+class AddExpenseActivity : AppCompatActivity(), CallBackReturn {
 
-    private          var day = 0
-    private lateinit var days : IntArray
+    private          var day = "0"
+    private lateinit var days : Array<String>
+
+    private lateinit var listBanks      : ArrayList<Bank>
+    private lateinit var bankSelected   : Bank
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_expense)
 
-        days = resources.getIntArray(R.array.days)
+        CRUDController.findAll( Bank(), supportFragmentManager , this, this)
+
+        days =  resources.getStringArray( R.array.days )
 
         ArrayAdapter.createFromResource(this, R.array.days, android.R.layout.simple_spinner_item)
             .also {adapter ->
@@ -41,6 +51,22 @@ class AddExpenseActivity : AppCompatActivity() {
             }
         }
 
+        list_bank_for_add_expense.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parentView          : AdapterView<*>?,
+                selectedItemView    : View?,
+                position            : Int,
+                id                  : Long
+            ) {
+                bankSelected = listBanks[position]
+            }
+
+            override fun onNothingSelected(parentView: AdapterView<*>?) {
+                //TODO implements
+            }
+        }
+
         btn_save_bank.setOnClickListener {
             saveExpense()
         }
@@ -50,6 +76,40 @@ class AddExpenseActivity : AppCompatActivity() {
 
     private fun saveExpense() {
 
+        val expense     = Expense()
+        expense.bank    = bankSelected
+        expense.dueDate = day.toString()
+        expense.name    = edt_expense_name  .text.toString()
+        expense.value   = edt_amount_expense.text.toString().toDouble()
+        expense.repeat  = chk_repeat.isChecked
+
+        CRUDController.create(expense, supportFragmentManager, this)
+    }
+
+    private fun inflateListBank( listBank : ArrayList<String> ) {
+        val adapter : ArrayAdapter<String> = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            listBank
+        )
+
+        list_bank_for_add_expense.adapter = adapter
+    }
+
+    private fun getListBankName( listBank: ArrayList<Bank> ) : ArrayList<String> {
+        val names = ArrayList<String>()
+
+        for ( bank in listBank ) {
+            names.add( bank.name )
+        }
+
+        return names
+    }
+
+    override fun callback(list: ArrayList<Any>){
+        listBanks           = list as ArrayList<Bank>
+        val listBankName    = getListBankName( listBanks )
+        inflateListBank(listBankName)
     }
 
 }
