c14c849199f850ec754073daf22527469a2f5deb
Guilherme <trintaeoitogc@gmail.com>
2021-01-21 22:47:22 -0300

    feito funcoes de buscar card e montado o grafico

--- app/src/main/java/com/simoes/expense/view/fragments/ChartsFragment.kt
+++ app/src/main/java/com/simoes/expense/view/fragments/ChartsFragment.kt
@@ -21,15 +21,59 @@ class ChartsFragment : Fragment(), CallBackReturn {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        
+        searchAllCard()
     }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_charts , container, false)
+        return inflater.inflate(R.layout.fragment_charts, container, false)
+    }
+
+    private fun configGraphs(list: List<Card>) {
+        val cardsAndValue   = java.util.ArrayList<String>()
+        val arr = BarGraphSeries<DataPoint>()
+
+        for ((index, card) in list.withIndex()) {
+            val name = card.name
+            var value = .0
+
+            for (expense in card.expenses) {
+                value += expense.value
+            }
+
+            val x = .0 + index
+            arr.appendData(DataPoint(x, value), true, Integer.MAX_VALUE)
+            cardsAndValue.add(name)
+        }
+
+        graph_bank.addSeries(arr)
+
+        arr.isDrawValuesOnTop   = true
+        arr.valuesOnTopColor    = Color.BLUE
+
+        graph_bank.viewport.isScrollable        = true
+        graph_bank.viewport.isYAxisBoundsManual = true
+        graph_bank.viewport.isXAxisBoundsManual = true
+
+        graph_bank.gridLabelRenderer.isHorizontalLabelsVisible = true
+        graph_bank.viewport.setMinY(0.0)
+        graph_bank.gridLabelRenderer.textSize = 40f
+        graph_bank.gridLabelRenderer.reloadStyles()
+        graph_bank.title = "Gastos por banco no mes"
+
+        arr.spacing    = 1
+
+        val staticLabelsFormatter = StaticLabelsFormatter(graph_bank)
+        staticLabelsFormatter.setHorizontalLabels(cardsAndValue.toArray( arrayOf() ))
+
+        graph_bank.gridLabelRenderer.labelFormatter = staticLabelsFormatter
+
+    }
+
+    private fun searchAllCard() {
+        CRUDController.findAll(Card(), fragmentManager!!, this, context!!)
     }
 
 }
