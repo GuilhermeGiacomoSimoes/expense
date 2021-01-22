c62477c500c55a3b4220dc03429768f05e479da0
Guilherme <trintaeoitogc@gmail.com>
2021-01-21 22:45:59 -0300

    feat: imports necessarios para o grafico

--- app/src/main/java/com/simoes/expense/view/fragments/ChartsFragment.kt
+++ app/src/main/java/com/simoes/expense/view/fragments/ChartsFragment.kt
@@ -1,11 +1,21 @@
 package com.simoes.expense.view.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.jjoe64.graphview.DefaultLabelFormatter
+import com.jjoe64.graphview.ValueDependentColor
+import com.jjoe64.graphview.helper.StaticLabelsFormatter
+import com.jjoe64.graphview.series.BarGraphSeries
+import com.jjoe64.graphview.series.DataPoint
 import com.simoes.expense.R
+import com.simoes.expense.controller.CRUDController
+import com.simoes.expense.helpers.CallBackReturn
+import com.simoes.expense.model.models.Card
+import kotlinx.android.synthetic.main.fragment_charts.*
 
 class ChartsFragment : Fragment() {
 
