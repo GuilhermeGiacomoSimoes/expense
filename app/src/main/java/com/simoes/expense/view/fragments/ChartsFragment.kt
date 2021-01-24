a51709b5354154426a774c7bfa1e2ab5a8457d67
Guilherme <trintaeoitogc@gmail.com>
2021-01-24 10:50:23 -0300

    fix: tirado parte comentada do codigo desnecessaria

--- app/src/main/java/com/simoes/expense/view/fragments/ChartsFragment.kt
+++ app/src/main/java/com/simoes/expense/view/fragments/ChartsFragment.kt
@@ -29,49 +29,6 @@ class ChartsFragment : Fragment(), CallBackReturn {
         return inflater.inflate(R.layout.fragment_charts, container, false)
     }
 
-//    private fun configGraphs( list: java.util.ArrayList<Card> ) {
-//        val banks = ArrayList<BarEntry>()
-//        val colors = ArrayList<Int>()
-//        val names= ArrayList<LegendEntry>()
-//
-//        for ((index, card) in list.withIndex()){
-//            var value = 0f
-//
-//            for (expense in card.expenses){
-//                value += expense.value.toFloat()
-//            }
-//
-//            colors.add(card.color)
-//
-//            val legend = LegendEntry()
-//            legend.label = card.name
-//            legend.formColor = card.color
-//
-//            names.add( legend )
-//            banks.add( BarEntry( index + .0f, value ) )
-//        }
-//
-//        val barDataSet = BarDataSet(banks, "banks")
-//        barDataSet.valueTextColor = Color.BLACK
-//        barDataSet.valueTextSize = 16f
-//        barDataSet.colors = colors
-//
-//        val legend = barChart.legend
-//        legend.setCustom( names )
-//        legend.form = Legend.LegendForm.LINE
-//        legend.textSize = 11f
-//        legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
-//        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
-//        legend.orientation = Legend.LegendOrientation.HORIZONTAL
-//        legend.setDrawInside(true)
-//
-//        val barData = BarData(barDataSet)
-//
-//        barChart.setFitBars(true)
-//        barChart.data = barData
-//        barChart.animateY(2000)
-//    }
-
     private fun configGraphs(list: ArrayList<Card>) {
         val visitors = ArrayList<PieEntry>()
 
