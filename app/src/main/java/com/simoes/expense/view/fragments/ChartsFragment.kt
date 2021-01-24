6885bb19eb01f4bc51f22d7efe8bd68fdb2d59c8
Guilherme <trintaeoitogc@gmail.com>
2021-01-24 12:25:45 -0300

    feat: criado função de criação do gráfico por categoria

--- app/src/main/java/com/simoes/expense/view/fragments/ChartsFragment.kt
+++ app/src/main/java/com/simoes/expense/view/fragments/ChartsFragment.kt
@@ -6,11 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.components.LegendEntry
 import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet
 import com.github.mikephil.charting.utils.ColorTemplate
 import com.simoes.expense.R
 import com.simoes.expense.controller.CRUDController
 import com.simoes.expense.helpers.CallBackReturn
+import com.simoes.expense.helpers.Helper
 import com.simoes.expense.model.models.Card
 import kotlinx.android.synthetic.main.fragment_charts.*
 
@@ -31,6 +34,48 @@ class ChartsFragment : Fragment(), CallBackReturn {
 
     private fun configGraphs(list: ArrayList<Card>) {
         configGraphCardSpend(list)
+        configGraphSpenderByCategory(list)
+    }
+
+    private fun configGraphSpenderByCategory(list: ArrayList<Card>) {
+        val banks   = ArrayList<BarEntry>()
+        val cards   = ArrayList<BarDataSet>()
+
+        val categorys = HashMap<String, Float>()
+
+        for (card in list) {
+
+            for (expense in card.expenses){
+                val value = categorys[expense.category.name]
+                if (value != null) {
+                    categorys[expense.category.name] = value + expense.value.toFloat()
+                }
+                else {
+                    categorys[expense.category.name] = expense.value.toFloat()
+                }
+            }
+        }
+
+        var index = 0
+        for ((key, value) in categorys){
+            banks.add( BarEntry( index + .0f, value) )
+
+            val barDataSet = BarDataSet(banks, Helper.getTranslateExpenseTypePortuguese(key))
+
+            barDataSet.valueTextColor = Color.BLACK
+            barDataSet.valueTextSize = 16f
+
+            cards.add(barDataSet)
+
+            index ++
+        }
+
+
+        val barData = BarData(cards.toList())
+
+        spenderByCategory.setFitBars(true)
+        spenderByCategory.data = barData
+        spenderByCategory.animateY(2000)
     }
 
     private fun configGraphCardSpend(list: ArrayList<Card>) {
