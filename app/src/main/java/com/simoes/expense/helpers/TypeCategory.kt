7767009966ee8b1ba209e326ad5806fd3cb54f2f
Guilherme <trintaeoitogc@gmail.com>
2021-01-24 12:23:53 -0300

    feat: criado enum de tipo de categoria

--- app/src/main/java/com/simoes/expense/helpers/TypeCategory.kt
+++ app/src/main/java/com/simoes/expense/helpers/TypeCategory.kt
@@ -1,4 +1,15 @@
 package com.simoes.expense.helpers
 
 enum class TypeCategory {
+    PUB,
+    RESTAURANT,
+    SUPERMARKET,
+    ALCOHOLIC_BEVERAGES,
+    TICKETS,
+    FOOD,
+    INTERNET,
+    WATER,
+    ENERGY,
+    FURNITURE,
+    OTHER
 }
