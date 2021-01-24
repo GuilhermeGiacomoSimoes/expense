fa1f738aa6e81035765f5bd4f2c201d04d233a26
Guilherme <trintaeoitogc@gmail.com>
2021-01-24 10:46:59 -0300

    feat: feito os graficos com pizza agora

--- app/src/main/java/com/simoes/expense/view/fragments/ChartsFragment.kt
+++ app/src/main/java/com/simoes/expense/view/fragments/ChartsFragment.kt
@@ -6,9 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.utils.ColorTemplate
 import com.simoes.expense.R
 import com.simoes.expense.controller.CRUDController
 import com.simoes.expense.helpers.CallBackReturn
@@ -30,31 +29,70 @@ class ChartsFragment : Fragment(), CallBackReturn {
         return inflater.inflate(R.layout.fragment_charts, container, false)
     }
 
-    private fun configGraphs( list: java.util.ArrayList<Card> ) {
-        val banks = ArrayList<BarEntry>()
-        val colors = ArrayList<Int>()
+//    private fun configGraphs( list: java.util.ArrayList<Card> ) {
+//        val banks = ArrayList<BarEntry>()
+//        val colors = ArrayList<Int>()
+//        val names= ArrayList<LegendEntry>()
+//
+//        for ((index, card) in list.withIndex()){
+//            var value = 0f
+//
+//            for (expense in card.expenses){
+//                value += expense.value.toFloat()
+//            }
+//
+//            colors.add(card.color)
+//
+//            val legend = LegendEntry()
+//            legend.label = card.name
+//            legend.formColor = card.color
+//
+//            names.add( legend )
+//            banks.add( BarEntry( index + .0f, value ) )
+//        }
+//
+//        val barDataSet = BarDataSet(banks, "banks")
+//        barDataSet.valueTextColor = Color.BLACK
+//        barDataSet.valueTextSize = 16f
+//        barDataSet.colors = colors
+//
+//        val legend = barChart.legend
+//        legend.setCustom( names )
+//        legend.form = Legend.LegendForm.LINE
+//        legend.textSize = 11f
+//        legend.verticalAlignment = Legend.LegendVerticalAlignment.BOTTOM
+//        legend.horizontalAlignment = Legend.LegendHorizontalAlignment.LEFT
+//        legend.orientation = Legend.LegendOrientation.HORIZONTAL
+//        legend.setDrawInside(true)
+//
+//        val barData = BarData(barDataSet)
+//
+//        barChart.setFitBars(true)
+//        barChart.data = barData
+//        barChart.animateY(2000)
+//    }
 
-        for ((index, card) in list.withIndex()){
-            var value = 0f
+    private fun configGraphs(list: ArrayList<Card>) {
+        val visitors = ArrayList<PieEntry>()
 
+        for (card in list){
             for (expense in card.expenses){
-                value += expense.value.toFloat()
+                visitors.add(PieEntry(expense.value.toFloat(), card.name))
             }
-
-            colors.add(card.color)
-            banks.add( BarEntry( index + .0f, value ) )
         }
-        
-        val barDataSet = BarDataSet(banks, "banks")
-        barDataSet.valueTextColor = Color.BLACK
-        barDataSet.valueTextSize = 16f
-        barDataSet.colors = colors
 
-        val barData = BarData(barDataSet)
+        val pieDataSet = PieDataSet(visitors, "")
+        pieDataSet.colors = ColorTemplate.COLORFUL_COLORS.toMutableList()
+        pieDataSet.valueTextColor = Color.BLACK
+        pieDataSet.valueTextSize = 16f
 
-        barChart.setFitBars(true)
-        barChart.data = barData
-        barChart.animateY(2000)
+        val pieData = PieData( pieDataSet )
+
+        cardSpend.data = pieData
+        cardSpend.description.isEnabled = true
+        cardSpend.centerText = "Visitors"
+        cardSpend.animate()
+        cardSpend.invalidate()
     }
 
     private fun searchAllCard() {
@@ -65,14 +103,4 @@ class ChartsFragment : Fragment(), CallBackReturn {
         val listCard = list as ArrayList<Card>
         configGraphs(listCard)
     }
-
-    private fun hexToRGB(hex : Int): Array<Int> {
-        return arrayOf( getRed(hex), getGreen(hex), getBlue(hex) )
-    }
-    private fun getRed(hex : Int) = hex shr 16 and 0xFF
-
-    private fun getGreen(hex : Int) = hex shr 8 and 0xFF
-
-    private fun getBlue(hex : Int) = hex and 0xFF
-
 }
