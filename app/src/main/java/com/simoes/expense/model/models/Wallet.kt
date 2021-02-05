b12eed1bf9b976b897616d2ddca2152d93aed7d4
Guilherme <trintaeoitogc@gmail.com>
2021-02-04 21:05:04 -0300

    fix: alinhados

--- app/src/main/java/com/simoes/expense/model/models/Wallet.kt
+++ app/src/main/java/com/simoes/expense/model/models/Wallet.kt
@@ -3,6 +3,6 @@ package com.simoes.expense.model.models
 import java.io.Serializable
 
 class Wallet : Serializable, Object() {
-    var uui = "d60bb62b-1353-409b-a07b-bc5e8f27b68c"
-    var amount = .0
+    var uui     = "d60bb62b-1353-409b-a07b-bc5e8f27b68c"
+    var amount  = .0
 }
