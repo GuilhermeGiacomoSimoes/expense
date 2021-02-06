7c916b67e8de07503473b266c10c6418a6544b16
Guilherme <trintaeoitogc@gmail.com>
2021-02-05 21:15:31 -0300

    feat: criado as funções de callback e o createWallet

--- app/src/main/java/com/simoes/expense/view/activitys/MainActivity.kt
+++ app/src/main/java/com/simoes/expense/view/activitys/MainActivity.kt
@@ -3,12 +3,17 @@ package com.simoes.expense.view.activitys
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.simoes.expense.R
+import com.simoes.expense.controller.CRUDController
+import com.simoes.expense.helpers.CallBackReturn
+import com.simoes.expense.helpers.Helper
+import com.simoes.expense.model.models.Wallet
 import com.simoes.expense.view.fragments.ChartsFragment
 import com.simoes.expense.view.fragments.ExpenseFragment
 import com.simoes.expense.view.fragments.MoreFragment
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CallBackReturn {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,5 +43,23 @@ class MainActivity : AppCompatActivity() {
         ft.replace(R.id.container, MoreFragment())
         ft.commit()
         super.onResume()
+    }
+
+    private fun createWallet() : Wallet {
+        val wallet = Wallet()
+        wallet.amount = .0
+
+        return wallet
+    }
+
+    override fun callback(list: ArrayList<Any>) {
+        if ( list.isNullOrEmpty() ) {
+            val wallet = createWallet()
+            CRUDController.create(wallet, supportFragmentManager, this, this)
+        }
+    }
+
+    override fun callback(isSuccess: Boolean) {
+        Helper.persistData(Helper.WALLET_CREATED, this, isSuccess.toString())
     }
 }
