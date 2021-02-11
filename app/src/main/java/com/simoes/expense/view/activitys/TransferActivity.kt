9ca46e66d08774f32229421467c03e72c8728193
Guilherme <trintaeoitogc@gmail.com>
2021-02-10 21:34:19 -0300

    feat: adicionado a classe R ao kt

--- app/src/main/java/com/simoes/expense/view/activitys/TransferActivity.kt
+++ app/src/main/java/com/simoes/expense/view/activitys/TransferActivity.kt
@@ -2,6 +2,7 @@ package com.simoes.expense.view.activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.simoes.expense.R
 
 class TransferActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
