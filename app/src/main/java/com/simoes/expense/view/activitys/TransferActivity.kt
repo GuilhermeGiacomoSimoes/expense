558dbde38b78fe774c6016b62c264379d3da8070
Guilherme <trintaeoitogc@gmail.com>
2021-02-10 23:12:41 -0300

    feat: feito o evento de click do botão transferir com todas as verificações

--- app/src/main/java/com/simoes/expense/view/activitys/TransferActivity.kt
+++ app/src/main/java/com/simoes/expense/view/activitys/TransferActivity.kt
@@ -5,28 +5,64 @@ import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.simoes.expense.R
 import com.simoes.expense.controller.CRUDController
 import com.simoes.expense.helpers.CallBackReturn
 import com.simoes.expense.model.models.Card
+import com.simoes.expense.view.fragments.FeedbackDialog
 import kotlinx.android.synthetic.main.activity_transfer.*
 import java.util.ArrayList
 
 class TransferActivity : AppCompatActivity(), CallBackReturn {
 
-    private lateinit var cardDe     : Card
-    private lateinit var cardPara   : Card
-    private lateinit var cards      : ArrayList<Card>
+    private var cardDe     = Card()
+    private var cardPara   = Card()
+    private var cards      = ArrayList<Card>()
+    private var countCallback = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transfer)
 
         findCards()
-        buildClickEventSpinners()
+        buildClickEvents()
     }
 
-    private fun buildClickEventSpinners() {
+    private fun verifyValue() = edt_value_transfer.text.toString().isEmpty()
+
+    private fun buildClickEvents() {
+
+        btn_transfer.setOnClickListener {
+            loading_transfer.visibility = View.VISIBLE
+            txt_btn_transfer.visibility = View.GONE
+
+            if ( cardDe != cardPara ){
+                if ( verifyValue() ) {
+                    loading_transfer.visibility = View.GONE
+                    txt_btn_transfer.visibility = View.VISIBLE
+                    FeedbackDialog.showDialog(supportFragmentManager, "Preencha o campo de valor", "Erro")
+                } else{
+                    val value = edt_value_transfer.text.toString().toDouble()
+                    if ( cardDe.balance < value) {
+                        loading_transfer.visibility = View.GONE
+                        txt_btn_transfer.visibility = View.VISIBLE
+                        FeedbackDialog.showDialog(supportFragmentManager, "O saldo do banco de origem não é o suficiente", "Erro")
+                    } else {
+                        cardDe.balance -= value
+                        cardPara.balance += value
+
+                        CRUDController.update( cardDe, supportFragmentManager, this, this )
+                        CRUDController.update( cardPara, supportFragmentManager, this, this )
+                    }
+                }
+            } else {
+                loading_transfer.visibility = View.GONE
+                txt_btn_transfer.visibility = View.VISIBLE
+                FeedbackDialog.showDialog(supportFragmentManager, "O banco de origem não pode ser igualao banco de destino", "Erro")
+            }
+        }
+
         spn_de.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(
                 parent: AdapterView<*>?,
