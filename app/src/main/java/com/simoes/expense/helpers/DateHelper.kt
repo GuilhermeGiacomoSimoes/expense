2c757bc0a6a8a6088240e9aaa1b9f8fc762ed644
Guilherme <trintaeoitogc@gmail.com>
2021-02-13 16:00:33 -0300

    fix: tirado funções não usadas do DateHelper e tirado os segundos da formatação da data

--- app/src/main/java/com/simoes/expense/helpers/DateHelper.kt
+++ app/src/main/java/com/simoes/expense/helpers/DateHelper.kt
@@ -12,33 +12,13 @@ class DateHelper {
             return getDateNow()
         }
 
-        //ex 30/08/2020
-        fun nowOnlyDate() : String {
-            return getDateNow().split(" ")[0]
-        }
-
-        //ex 06:26:00
-        fun nowOnlyHours() : String  {
-            return getDateNow().split(" ")[1]
-        }
-
         fun nowMilliseconds() : Long  {
             return System.currentTimeMillis()
         }
 
-        fun nowMonth() : String {
-            val month = Date( getDateNow() ).month + 1
-            return month.toString()
-        }
-
-        fun nowYear() : String {
-            val year = Date(getDateNow()).year
-            return year.toString()
-        }
-
         @SuppressLint("SimpleDateFormat")
         private fun getDateNow() : String {
-            return SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format( Date() )
+            return SimpleDateFormat("dd/MM/yyyy HH:mm").format( Date() )
         }
     }
 
