e83a6fefb45e7f4c6bc754547feb566146b02878
Guilherme <trintaeoitogc@gmail.com>
2021-02-14 16:23:26 -0300

    feat: feito o when para todos os typeCategory

--- app/src/main/java/com/simoes/expense/view/adapters/ExpenseAdapter.kt
+++ app/src/main/java/com/simoes/expense/view/adapters/ExpenseAdapter.kt
@@ -72,10 +72,37 @@ class ExpenseAdapter(private var listExpense: ArrayList<Expense>, private var co
     }
 
     private fun getImageCardOrMoney( expense : Expense ) : Int {
-        return if ( expense.typeExpense == TypeExpense.MONEY ){
-            R.drawable.ic_money_expense
-        } else {
-            R.drawable.ic_card_expense
+        return when (expense.category) {
+            TypeCategory.PUB -> {
+                R.drawable.ic_pub
+            }
+            TypeCategory.SUPERMARKET -> {
+                R.drawable.ic_supermarket
+            }
+            TypeCategory.ALCOHOLIC_BEVERAGES -> {
+                R.drawable.ic_alcoholic_beverages
+            }
+            TypeCategory.TICKETS -> {
+                R.drawable.ic_tickets
+            }
+            TypeCategory.FOOD -> {
+                R.drawable.ic_food
+            }
+            TypeCategory.INTERNET -> {
+                R.drawable.ic_internet
+            }
+            TypeCategory.WATER -> {
+                R.drawable.ic_water
+            }
+            TypeCategory.ENERGY -> {
+                R.drawable.ic_energy
+            }
+            TypeCategory.FURNITURE -> {
+                R.drawable.ic_furniture
+            }
+            else -> {
+                R.drawable.ic_other
+            }
         }
     }
 }
