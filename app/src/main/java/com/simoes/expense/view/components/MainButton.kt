28215702a0090f609d50ea1a2a6a12a2d6a9eb7e
Guilherme <trintaeoitogc@gmail.com>
2021-02-20 16:42:32 -0300

    fix: trocado icones

--- app/src/main/java/com/simoes/expense/view/components/MainButton.kt
+++ app/src/main/java/com/simoes/expense/view/components/MainButton.kt
@@ -1,7 +1,9 @@
-package com.simoes.expense.view.Components
+package com.simoes.expense.view.components
 
+import android.content.Context
+import android.util.AttributeSet
 import android.view.View
 
-class MainButton : View() {
+class MainButton(context : Context, attrs :AttributeSet) : View( context, attrs ) {
 
 }
