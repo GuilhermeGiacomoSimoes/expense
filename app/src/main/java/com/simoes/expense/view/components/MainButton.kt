6962db13b55af0b8659f8e12ce80f9e00ff1a3a3
Guilherme <trintaeoitogc@gmail.com>
2021-02-25 21:50:56 -0300

    fix: removido view inutilizada

--- app/src/main/java/com/simoes/expense/view/components/MainButton.kt
+++
@@ -1,9 +0,0 @@
-package com.simoes.expense.view.components
-
-import android.content.Context
-import android.util.AttributeSet
-import android.view.View
-
-class MainButton(context : Context, attrs :AttributeSet) : View( context, attrs ) {
-
-}
